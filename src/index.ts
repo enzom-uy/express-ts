7d7d2ab6e0e7e3f91d1f0b8060347cbac082391f
enzom-uy <enzom.dev@gmail.com>
2022-12-16 10:34:10 -0300

    feat(template): initial files

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT as string;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Trunk Notes API - Index");
+});
+
+app.listen(port, () => {
+  console.debug(`⚡️[server]: Server is running at port ${port}`);
+});
